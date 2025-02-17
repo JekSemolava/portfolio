afc7a3ea58e6e966a27428feeaf4d4e879b6bc59
JekSemolava <jason.semolava@gmail.com>
2025-02-17 16:12:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import express from "express";
-/*import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));*/
-
 const app = express();
-/*const port = 3000;*/
 
-/*app.use(bodyParser.urlencoded({extended:true}));*/
 var userIsLoggedIn = false;
 app.use(express.static("public"));
 app.use(express.json());
