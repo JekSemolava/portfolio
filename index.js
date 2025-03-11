7182afa10f104445a5a6624bbb727f3bf79841b7
JekSemolava <jason.semolava@gmail.com>
2025-03-11 16:15:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
+const port = 3000;
 
 var userIsLoggedIn = false;
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-app.use(express.json());
 app.use(passwordCheck);
 
 function logout() {
-  window.location.href = "/portfolio/public/login";
+  // window.location.href = "/logout";
+  window.location.href = "/login";
 }
 
 function login() {
+  // window.location.href = "/login";
   window.location.href = "/portfolio/public/index";
 }
 
@@ -27,49 +35,35 @@ function passwordCheck(req, res, next) {
 
 app.get("/", (req, res) => {
    if (userIsLoggedIn) {
-      res.sendFile(__dirname + "/portfolio/public/index.html");
+      res.sendFile(__dirname + "/public/index.html");
     }
 });
 
 
-app.post("/portfolio/public/index", (req, res) => {
+app.post("/check", (req, res) => {
     if (userIsLoggedIn) {
       res.sendFile(__dirname + "/public/index.html");
-    } 
-    else {
+    } else {
       res.sendFile(__dirname + "/public/login.html");
     }
   });
 
 app.get("/logout", (req, res) => {
   userIsLoggedIn = false;
+  // res.redirect("/");
   res.sendFile(__dirname + "/public/login.html");
 });
 
 app.get("/portfolio/public/login", (req, res) => {
   userIsLoggedIn = false;
+  // res.redirect("/");
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post('/portfolio/public/check', (req, res) => {
-  const password = req.body.password;
-  if (password === 'user123') {
-    userIsLoggedIn = true;
-    res.json({ success: true });
-    res.sendFile(__dirname + "/index.html");
-  } else {
-    userIsLoggedIn = false;
-    res.json({ success: false });
-    res.sendFile(__dirname + "/login.html");
-  }
-});
-
-app.get('/portfolio/public/index', (req, res) => {
-  if (userIsLoggedIn) {
-    res.sendFile(__dirname + '/public/index.html');
-  } else {
-    res.redirect('/portfolio/public/login');
-  }
+app.get("/portfolio/public/index", (req, res) => {
+  userIsLoggedIn = true;
+  // res.redirect("/");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
