381f5eb54e95db4e6abab32a84ac2b55cc0c8179
JekSemolava <jason.semolava@gmail.com>
2025-04-08 21:56:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+
+var userIsLoggedIn = false;
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(passwordCheck);
+
+
+function passwordCheck(req, res, next) {
+    const password = req.body["password"];
+    if (password === "user123") {
+        userIsLoggedIn = true;
+      } else {
+        userIsLoggedIn = false;
+      }
+    next();
+  }
+
+
+app.get("/logout", (req, res) => {
+  if (userIsLoggedIn) {
+    res.render("index.ejs");
+   } else {
+    res.render("login.ejs");
+ }
+});
+
+app.get("/login", (req, res) => {
+  if (userIsLoggedIn) {
+    res.render("login.ejs");
+   } else {
+    res.render("index.ejs");
+ }
+});
+
+
+app.post("/check", (req, res) => {
+    if (userIsLoggedIn) {
+      res.render("index.ejs");
+    } else {
+      res.render("login.ejs");
+    }
+  });
+
+app.get("/", (req, res) => {
+  userIsLoggedIn = false;
+  res.render("login.ejs");
+});
+
+
+app.get("/home", (req, res) => {
+  userIsLoggedIn = true;
+  res.render("index.ejs");
+});
+
+app.get("/blog", (req, res) => {
+  userIsLoggedIn = true;
+  res.render("blog.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
